6ca169f1e707b0cbbde8da5937bfb877d6ee77e2
Nicolas Messer <nme@nicolasmesser.ch>
2023-11-27 15:07:07 +0100

    Facebook OpenGraph

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_url_options {
+    host: ENV["DOMAIN"] || "localhost:3000"
+    }
+  end
+
 end
