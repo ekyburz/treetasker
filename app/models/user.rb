e391e6009ad761d3d795970cf896df0ef777baf1
EtienneKyburz <114604643+ekyburz@users.noreply.github.com>
2023-11-28 12:37:15 +0100

    changes to the seedfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tasks, dependent: :destroy
+  has_one :garden, dependent: :destroy
 end
