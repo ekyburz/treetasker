1afd0f8a20a7ee56cc3d9d2032aea4cc984baf15
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2023-11-29 18:07:29 +0100

    Add dotenv-rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :create_user_profile
-
-  private
-
-  def create_user_profile
-    Profile.create(user: self)
-  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
