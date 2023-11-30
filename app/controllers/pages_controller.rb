571d7d1dd56fd646311788561a6846ad4259f778
EtienneKyburz <114604643+ekyburz@users.noreply.github.com>
2023-11-30 11:34:22 +0100

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home; end
+  def home
+    @user = current_user
+  end
 
   def profile
     @user = current_user
