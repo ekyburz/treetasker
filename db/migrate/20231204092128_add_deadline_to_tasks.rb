d9c7fd3f901cb10c38f31672d0e49e6a31e7ab58
Nicolas Messer <nme@nicolasmesser.ch>
2023-12-06 12:39:54 +0100

    Garden Fixes for Etienne

--- db/migrate/20231204092128_add_deadline_to_tasks.rb
+++ db/migrate/20231204092128_add_deadline_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddDeadlineToTasks < ActiveRecord::Migration[7.1]
   def change
-    add_column :tasks, :deadline, :datetime
+    add_column :tasks, :deadline, :datetime, null: false, default: DateTime.now
   end
 end
